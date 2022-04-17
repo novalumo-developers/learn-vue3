b416d9fbbc17fc0ed833f70f58d1de5a8d6de683
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-04-17 15:33:12 +0900

    feat: basic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+<script setup lang="ts"></script>
+<script lang="ts">
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      message: "Hello Vue!",
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+  mounted() {
+    alert("mounted");
+  },
+};
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    {{ message }}
+    {{ count }}
+    <button @click="increment">Increment</button>
   </main>
 </template>
